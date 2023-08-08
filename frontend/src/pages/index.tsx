9d36f56cb855e96a5217b7e9055e8ceb5d6903a9
jwun95 <jwun95@naver.com>
2023-08-08 18:46:45 +0900

    Feat: Home - Header

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from "react";
+import styled from "@emotion/styled";
+import Link from "next/link";
+import Logo from "../../public/Logo.svg";
+import Image from "next/image";
+import { AiOutlineSetting } from "react-icons/ai";
+import { IconContext } from "react-icons";
 
 export default function Home() {
-	return (
-		<div>Home</div>
-	)
+  return (
+    <div>
+      <Header>
+        <Link href="/">
+          <Image src={Logo} alt="main-logo" />
+        </Link>
+        <IconContext.Provider
+          value={{
+            color: "gray",
+            size: "24",
+          }}
+        >
+          {" "}
+          <Link href="/setting">
+            <AiOutlineSetting />
+          </Link>
+        </IconContext.Provider>
+      </Header>
+    </div>
+  );
 }
 
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
