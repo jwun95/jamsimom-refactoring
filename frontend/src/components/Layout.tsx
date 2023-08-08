35044df15dbf54f63a246233ed3fbbfbcc6d4b65
jwun95 <jwun95@naver.com>
2023-08-08 13:11:31 +0900

    Feat: Navbar layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import styled from "@emotion/styled";
 
 type Props = {
   children: React.ReactNode;
@@ -7,9 +8,28 @@ type Props = {
 
 export default function Layout(props: Props) {
   return (
-    <div>
-			{props.children}
-      <Navbar />
-    </div>
+    <Container>
+      <MainContainer>
+        <ContentContainer>{props.children}</ContentContainer>
+        <Navbar />
+      </MainContainer>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  background-color: #f5f5f5;
+`;
+
+const MainContainer = styled.div`
+  background-color: #ffff;
+  max-width: 440px;
+  min-width: 360px;
+  margin: auto;
+  height: 100vh;
+  padding: 16px 16px 54px 16px;
+`;
+
+const ContentContainer = styled.main`
+  height: 100%;
+`;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,40 @@
+import { AiFillHome, AiOutlineUser } from "react-icons/ai";
+import { BsSearch } from "react-icons/bs";
+import styled from "@emotion/styled";
+
 export default function Navbar() {
-	return (
-		<nav>navbar</nav>
-	)
+  return (
+    <Nav>
+      <Items>
+        <Item>
+          <AiFillHome />
+        </Item>
+        <Item>
+          <BsSearch />
+        </Item>
+        <Item>
+          <AiOutlineUser />
+        </Item>
+      </Items>
+    </Nav>
+  );
 }
+
+const Nav = styled.nav`
+  position: sticky;
+  width: inherit;
+  bottom: 0px;
+  z-index: 10;
+  height: 56px;
+  background-color: red;
+`;
+
+const Items = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 8px;
+`;
+
+const Item = styled.li`
+  display: flex;
+`;
